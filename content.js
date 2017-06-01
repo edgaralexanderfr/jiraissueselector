d0e3baecd7fcdefae24dc30afd045d8d4c7b017f
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2017-06-01 08:40:38 -0430

    Ya se pueden seleccionar issues desde la vista de sprints.

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     var keyVal     = document.getElementById('key-val');
     var summaryVal = document.getElementById('summary-val');
+
+    if (keyVal == null) {
+    	keyVal = document.getElementById('issuekey-val');
+
+    	if (keyVal != null) {
+    		keyVal = keyVal.children[0];
+    	}
+    }
     
     if (keyVal == undefined || summaryVal == undefined || keyVal['innerText'] == undefined || summaryVal['innerText'] == undefined) {
     	alert('Error: Debes abrir un issue de Jira para correr esta extensión.');
